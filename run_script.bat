eb7a1f4eb6469cd57cf28629578f87eb8bded0aa
Yan <yan.mezhov@yandex.ru>
2021-05-17 17:49:50 +0300

    clear run scripts and setting by unnececary settings

--- run_script.bat
+++ run_script.bat
@@ -3,5 +3,5 @@ set PROJECT=%1
 set BUILD_TYPE=%2
 
 cd Builds/win/%PROJECT%
-%PROJECT%-%BUILD_TYPE%.exe "-project -%PROJECT%" "-showBones -yes" "-showGoal -yes" "-showMatchingStatistic -yes" "-loadDataBaseFromFBX -no"
+%PROJECT%-%BUILD_TYPE%.exe "-project -%PROJECT%" "-loadDataBaseFromFBX -no"
 cd ../../..
