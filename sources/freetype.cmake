23862b7a052343e3fa188fc41960605fa0d9fed0
yan.mezhov <yan.mezhov@yandex.ru>
2022-07-24 22:13:58 +0500

    import freetype

---
+++ sources/freetype.cmake
@@ -0,0 +1,13 @@
+
+if(WIN32)
+  set(ADDITIONAL_LIBS ${ADDITIONAL_LIBS} freetype)
+
+  add_library(freetype STATIC IMPORTED)
+  set_target_properties(freetype PROPERTIES
+          IMPORTED_LOCATION_DEBUG "${CMAKE_BINARY_DIR}/../freetype.lib"
+          IMPORTED_LOCATION_RELWITHDEBINFO "${CMAKE_BINARY_DIR}/../freetype.lib"
+          IMPORTED_LOCATION_RELEASE "${CMAKE_BINARY_DIR}/../freetype.lib"
+          LINKER_LANGUAGE CXX)
+else()
+  find_package(freetype REQUIRED)
+endif()
