ab40bd8cd1280a13fde03e93dcdfbe152446f616
yan.mezhov <yan.mezhov@yandex.ru>
2023-01-04 19:01:55 +0100

    remove old ecs lib

--- sources/old_ecs/ecs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "ecs_core.h"
-#include "ecs_event.h"
-#include "ecs_tag.h"
-#define SYSTEM(...)static __forceinline void
-#define QUERY(...)
-#define EVENT(...)static __forceinline void
--- sources/old_ecs/ecs_event.cpp
+++
@@ -1,8 +0,0 @@
-#include "ecs_core.h"
-#include "ecs_event.h"
-#include "ecs_event_registration.h"
-
-ECS_EVENT_REGISTER(OnSceneCreated, ecs::OnSceneCreated)
-ECS_EVENT_REGISTER(OnSceneDestroy, ecs::OnSceneDestroy)
-ECS_EVENT_REGISTER(OnEntityCreated, ecs::OnEntityCreated)
-ECS_EVENT_REGISTER(OnEntityDestroyed, ecs::OnEntityDestroyed)
