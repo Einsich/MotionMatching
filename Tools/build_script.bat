4b963afd8433e11708c7f04d92fd7d0b1fcd799c
yan.mezhov <yan.mezhov@yandex.ru>
2023-01-08 18:01:18 +0100

    das aot integraton pipeline

--- Tools/build_script.bat
+++ Tools/build_script.bat
@@ -2,11 +2,11 @@
 set PROJECT=%1
 set BUILD_TYPE=%2
 set CMAKE=%3
-set AOT_COMPILER=%4
+set DAS_AOT=%4
 
 cd sources
 if "%CMAKE%"=="yes" (
-    cmake -G Ninja -DGAME=%PROJECT% -DBUILD_TYPE=%BUILD_TYPE% -DAOT_COMPILER=%AOT_COMPILER% -B ../Builds/%BUILD_TYPE%
+    cmake -G Ninja -DGAME=%PROJECT% -DBUILD_TYPE=%BUILD_TYPE% -DDAS_AOT=%DAS_AOT% -B ../Builds/%BUILD_TYPE%
 )
 
 cd ../Builds/%BUILD_TYPE%
