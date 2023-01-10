420bf5353663ca355428b4e60f33b018ed07fbd0
yan.mezhov <yan.mezhov@yandex.ru>
2023-01-10 02:23:01 +0100

    use type fabrics

--- sources/engine/ecs/registration.h
+++ sources/engine/ecs/registration.h
@@ -3,10 +3,32 @@
 #include <ecs/user_function_registration.h>
 #include "user_function_registration.h"
 
-#define ECS_REGISTER_TYPE(TYPE, NAME, TYPE_TIP, ...)\
-ECS_TYPE_REGISTRATION(TYPE, NAME, TYPE_TIP, template_blk_constructor<TYPE>, __VA_ARGS__)\
+
+template <typename T>
+struct RegistrationHelper
+{
+  struct CustomTypeFabric final : public ecs::DefaultTypeFabric<T>
+  {
+    CustomTypeFabric(ecs::Type type, bool has_awaiter = false):
+      ecs::DefaultTypeFabric<T>(type, true, has_awaiter)
+    {}
+
+    void prefab_constructor(void *raw_memory, const ecs::ComponentPrefab &prefab, bool r_value) const override
+    {
+      return template_blk_constructor<T>(raw_memory, prefab, r_value);
+    }
+  } fabric;
+
+  RegistrationHelper(const char *name, ecs::Type type) : fabric(type)
+  {
+    ecs::type_registration<T>(name, &fabric);
+  }
+};
+
+#define ECS_REGISTER_TYPE(TYPE, NAME, TYPE_TIP)\
+static RegistrationHelper<TYPE> __CONCAT__(registrator_helper, __COUNTER__) (NAME, TYPE_TIP);\
 ECS_USER_TYPE_REGISTRATION(TYPE, get_user_functions<TYPE>())
 
-#define ECS_REGISTER_TYPE_AND_VECTOR(TYPE, NAME, TYPE_TIP, ...)\
-ECS_REGISTER_TYPE(TYPE, NAME, TYPE_TIP, __VA_ARGS__)\
-ECS_REGISTER_TYPE(ecs::vector<TYPE>, "vector<" NAME ">", ecs::TrivialMoveConstructor, __VA_ARGS__)
+#define ECS_REGISTER_TYPE_AND_VECTOR(TYPE, NAME, TYPE_TIP)\
+ECS_REGISTER_TYPE(TYPE, NAME, TYPE_TIP)\
+ECS_REGISTER_TYPE(ecs::vector<TYPE>, "vector<" NAME ">", ecs::TrivialMoveConstructor)
