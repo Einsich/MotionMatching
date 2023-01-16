75cb1d9e05a683ecd0c0fe5c8befbc4799c21b8b
yan.mezhov <yan.mezhov@yandex.ru>
2023-01-16 01:35:22 +0100

    upgrade thread pool

--- sources/engine/parallel/thread_pool.cpp
+++ sources/engine/parallel/thread_pool.cpp
@@ -4,6 +4,8 @@
 #include <condition_variable>
 #include <atomic>
 #include <thread>
+#include <string>
+#include <profiler.h>
 
 struct thread_pool {
   // the mutex, condition variable and deque form a single
@@ -14,6 +16,7 @@ struct thread_pool {
   std::queue<std::function<void()>> tasks;
 
   std::vector<std::thread> workers;
+  int busyWorkers = 0;
   bool terminatePool;
 
   void add_job(std::function<void()>&& f) {
@@ -27,9 +30,10 @@ struct thread_pool {
   thread_pool():terminatePool(false)
   {
     auto num_threads = std::thread::hardware_concurrency();
+    busyWorkers = num_threads;
     for (std::size_t i = 0; i < num_threads; ++i)
     {
-      workers.emplace_back(std::thread(&thread_pool::infinite_loop, this));
+      workers.emplace_back(std::thread(&thread_pool::infinite_loop, this, i));
     }
   }
 
@@ -46,18 +50,22 @@ struct thread_pool {
     workers.clear();
   }
 private:
-  void infinite_loop() {
+  void infinite_loop(std::size_t idx) {
+    std::string name = "Worker" + std::to_string(idx);
+    OPTICK_THREAD(name.c_str());
     while (true){
       std::function<void()> f;
       {
         // usual thread-safe queue code:
         std::unique_lock<std::mutex> l(m);
+        busyWorkers--;
         if (tasks.empty()){
           v.wait(l,[&]{return !tasks.empty() || terminatePool;});
         }
         if (terminatePool)
           return;
         f = std::move(tasks.front());
+        busyWorkers++;
         tasks.pop();
       }
       f();
@@ -70,4 +78,12 @@ static thread_pool threadPool;
 void add_job(std::function<void()>&& f)
 {
   threadPool.add_job(std::move(f));
+}
+
+void wait_jobs()
+{
+  PROFILER_EVENT("thread_pool_wait")
+  while (!threadPool.tasks.empty() || threadPool.busyWorkers > 0)
+  {
+  }
 }
--- sources/engine/parallel/thread_pool.h
+++ sources/engine/parallel/thread_pool.h
@@ -2,6 +2,6 @@
 #include <functional>
 
 
-
+void wait_jobs();
 void add_job(std::function<void()>&& f);
 void add_main_thread_job(std::function<void()> &&job);
